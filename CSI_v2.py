59fe710aa7fe34c8a282ea0123e9f3664e931f42
klemzyman <150705488+klemzyman@users.noreply.github.com>
2023-11-29 20:36:06 +0100

    Add files via upload

---
+++ CSI_v2.py
@@ -0,0 +1,27 @@
+import json
+
+with open("suspects_3.json", "r") as sus_json:
+    suspects = json.loads(sus_json.read())
+
+with open("characteristics.json", "r") as char_json:
+    characteristics = json.loads(char_json.read())
+
+with open("dna.txt", "r") as dna_data:
+    dna = dna_data.read()
+
+char_matched = 0
+
+for suspect in suspects:
+    for characteristic in characteristics.keys():
+        char_value = suspects[suspect][characteristic].lower()
+        char_dna = characteristics[characteristic][char_value]
+        
+        if dna.find(char_dna) != -1:
+            char_matched += 1
+        else:
+            char_matched = 0
+            break
+    
+    if char_matched == len(characteristics):
+        print(suspect.title() + ' did it!!')
+        break
